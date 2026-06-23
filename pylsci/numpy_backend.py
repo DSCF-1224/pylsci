e39cb1164ab5731c13a2f993c85870def5f425c5
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-06-24 00:22:57 +0900

    refactor: extract normal equation construction helper

--- pylsci/numpy_backend.py
+++ pylsci/numpy_backend.py
@@ -5,6 +5,47 @@ import numpy as np
 from .result import Center, FittedCircle
 
 
+def _construct_normal_equation(x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Construct the normal equation for LSCI fitting.
+
+    Parameters
+    ----------
+    x
+        X coordinates.
+    y
+        Y coordinates.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        Normal-equation matrix and right-hand-side vector.
+    """
+
+    x2 = x * x
+    y2 = y * y
+
+    r2 = x2 + y2
+
+    sum_x1_y0 = np.sum(x)
+    sum_x0_y1 = np.sum(y)
+    sum_x1_y1 = np.sum(x * y)
+    sum_x2_y0 = np.sum(x2)
+    sum_x0_y2 = np.sum(y2)
+
+    matrix = np.array(
+        [
+            [sum_x2_y0, sum_x1_y1, sum_x1_y0],
+            [sum_x1_y1, sum_x0_y2, sum_x0_y1],
+            [sum_x1_y0, sum_x0_y1, float(x.size)]
+        ]
+    )
+
+    vector = np.array([np.sum(x * r2), np.sum(y * r2), np.sum(r2)])
+
+    return matrix, vector
+
+
 def fit(x: np.ndarray, y: np.ndarray) -> FittedCircle:
     """
     Fit a least-squares reference circle (LSCI) from a set of points.
@@ -36,24 +77,7 @@ def fit(x: np.ndarray, y: np.ndarray) -> FittedCircle:
     if size_x < 3:
         raise ValueError("at least 3 points are required")
 
-    x2 = x * x
-    y2 = y * y
-
-    r2 = x2 + y2
-
-    sum_x1_y0 = np.sum(x)
-    sum_x0_y1 = np.sum(y)
-    sum_x1_y1 = np.sum(x * y)
-    sum_x2_y0 = np.sum(x2)
-    sum_x0_y2 = np.sum(y2)
-
-    matrix = np.array(
-        [[sum_x2_y0, sum_x1_y1, sum_x1_y0],
-         [sum_x1_y1, sum_x0_y2, sum_x0_y1],
-         [sum_x1_y0, sum_x0_y1, float(size_x)]]
-    )
-
-    vector = np.array([np.sum(x * r2), np.sum(y * r2), np.sum(r2)])
+    matrix, vector = _construct_normal_equation(x, y)
 
     solution = np.linalg.solve(matrix, vector)
 
